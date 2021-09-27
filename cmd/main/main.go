6b3c94eb20ad9af9cb71f77d0852133798c4e694
vr009 <slavarianov@yandex.ru>
2021-09-27 19:56:47 +0300

    jwt middleware added, parsing and validation works, all func are in working state

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -24,7 +24,7 @@ func run() error {
 	r := mux.NewRouter()
 	srv := http.Server{Handler: r, Addr: fmt.Sprintf(":%s", "8080")}
 
-	conn := "port=5432 host=127.0.0.1 user=slavaryanov dbname=postgres password=postgres"
+	conn := ""
 
 	pool, err := pgxpool.Connect(context.Background(), conn)
 	if err != nil {
@@ -38,7 +38,6 @@ func run() error {
 
 	filmsHandler := filmsDelivery.FilmsHandler{}
 
-	os.Setenv("SECRET", "DEBUG")
 	auth := r.PathPrefix("/user").Subrouter()
 	{
 		auth.HandleFunc("/login", authHandler.Login).Methods(http.MethodPost)
