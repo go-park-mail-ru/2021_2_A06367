93cd9ebff791d0c8664d302e1e280cdebd98c14f
vr009 <slavarianov@yandex.ru>
2021-10-16 12:41:54 +0300

    58.9% coverage reached

--- internal/pkg/films/delivery/http/handlers_test.go
+++ internal/pkg/films/delivery/http/handlers_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/go-park-mail-ru/2021_2_A06367/internal/models"
 	"github.com/go-park-mail-ru/2021_2_A06367/internal/pkg/films"
 	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/require"
 	"log"
@@ -13,6 +14,21 @@ import (
 	"strings"
 	"testing"
 )
+
+//var testUsers = []models.User{
+//	{Id: uuid.New(), Login: "User A"},
+//	{Id: uuid.New(), Login: "User B"},
+//}
+//
+//var testActors = []models.Actors{
+//	{Id: uuid.New(), Name: "Burunov"},
+//	{Id: uuid.New(), Name: "Petrov"},
+//}
+//
+//var testFilms = []models.Film{
+//	{Id: uuid.New(), Title: "Policeman from Rublevka", Genres: []string{"Comedy"}, Year: 2015, Director: []string{"Director"}, Authors: []string{"author"}},
+//	{Id: uuid.New(), Title: "Mission Impossible", Genres: []string{"Triller"}},
+//}
 
 func TestFilmByGenre(t *testing.T) {
 
@@ -53,5 +69,64 @@ func TestFilmBySelection(t *testing.T) {
 
 	handler.FilmBySelection(w, r)
 	require.Equal(t, w.Code, http.StatusOK)
+	log.Print(w.Body.String())
+}
+
+func TestFilmsHandler_FilmByActor(t *testing.T) {
+	ctl := gomock.NewController(t)
+	defer ctl.Finish()
+
+	uid := uuid.New()
+
+	usecase := films.NewMockFilmsUsecase(ctl)
+	usecase.EXPECT().GetFilmsOfActor(models.Actors{Id: uid}).Times(1).Return([]models.Film{}, models.Okey)
+
+	handler := NewFilmsHandler(usecase)
+
+	r := httptest.NewRequest("GET", "/selection/user/personal", strings.NewReader(fmt.Sprint()))
+	r = mux.SetURLVars(r, map[string]string{
+		"actor_id": uid.String(),
+	})
+	w := httptest.NewRecorder()
+
+	handler.FilmByActor(w, r)
+	require.Equal(t, w.Code, http.StatusOK)
+	log.Print(w.Body.String())
+}
+
+func TestFilmsHandler_FilmById(t *testing.T) {
+	ctl := gomock.NewController(t)
+	defer ctl.Finish()
+
+	uid := uuid.New()
+
+	usecase := films.NewMockFilmsUsecase(ctl)
+	usecase.EXPECT().GetFilm(models.Film{Id: uid}).Times(1).Return(models.Film{}, models.Okey)
+
+	handler := NewFilmsHandler(usecase)
+
+	r := httptest.NewRequest("GET", "/film/"+uid.String(), strings.NewReader(fmt.Sprint()))
+	r = mux.SetURLVars(r, map[string]string{
+		"film_id": uid.String(),
+	})
+	w := httptest.NewRecorder()
+
+	handler.FilmById(w, r)
+	require.Equal(t, w.Code, http.StatusOK)
+	log.Print(w.Body.String())
+}
+
+func TestFilmsHandler_FilmsByUser(t *testing.T) {
+	ctl := gomock.NewController(t)
+	defer ctl.Finish()
+	usecase := films.NewMockFilmsUsecase(ctl)
+	handler := NewFilmsHandler(usecase)
+
+	r := httptest.NewRequest("GET", "/selection/user/personal", strings.NewReader(fmt.Sprint()))
+
+	w := httptest.NewRecorder()
+
+	handler.FilmsByUser(w, r)
+	require.Equal(t, w.Code, http.StatusBadRequest)
 	log.Print(w.Body.String())
 }
