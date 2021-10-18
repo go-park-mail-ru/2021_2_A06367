2359c1770b08d3e564d65e007659da854c8ac76b
Blackbackofficial <Chernov-Ivan.1997@yandex.ru>
2021-10-18 16:17:09 +0300

    Правка линтеров

--- internal/models/actors.go
+++ internal/models/actors.go
@@ -6,12 +6,12 @@ import (
 )
 
 type Actors struct {
-	Id					uuid.UUID 	`json:"id"`
-	Name				string    	`json:"name"`
-	Surname				string    	`json:"surname"`
-	Avatar				string		`json:"avatar"`
-	Height				float32		`json:"height"`
-	DateOfBirth			time.Time	`json:"date_of_birth"`
-	Description	string		`json:"description"`
-	Genres				[]string	`json:"genres"`
+	Id          uuid.UUID `json:"id"`
+	Name        string    `json:"name"`
+	Surname     string    `json:"surname"`
+	Avatar      string    `json:"avatar"`
+	Height      float32    `json:"height"`
+	DateOfBirth time.Time `json:"date_of_birth"`
+	Description string    `json:"description"`
+	Genres      []string  `json:"genres"`
 }
